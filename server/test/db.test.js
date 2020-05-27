692f14711325cf9fb9e072817eb0e9157437feb5
Michal Stachnik <michal@michals-mini.myfiosgateway.com>
2020-05-26 23:52:51 -0400

    Added tests to see if the mongodb instance is up and running

--- server/test/db.test.js
+++ server/test/db.test.js
@@ -1,14 +1,19 @@
 const { MongoClient } = require('mongodb');
 
+require('dotenv').config();
+
 describe('insert', () => {
   let connection;
   let db;
 
   beforeAll(async () => {
-    connection = await MongoClient.connect(global.__MONGO_URI__, {
-      useNewUrlParser: true,
-    });
-    db = await connection.db(global.__MONGO_DB_NAME__);
+    connection = await MongoClient.connect(
+      `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds253537.mlab.com:53537/guilty-eater`,
+      {
+        useNewUrlParser: true,
+      }
+    );
+    db = await connection.db('my-db');
   });
 
   afterAll(async () => {
@@ -16,14 +21,19 @@ describe('insert', () => {
     await db.close();
   });
 
-  it('should insert a doc into collection', async () => {
-    console.log('ran test');
-    // const users = db.collection('users');
-
-    // const mockUser = { _id: 'some-user-id', name: 'John' };
-    // await users.insertOne(mockUser);
-
-    // const insertedUser = await users.findOne({ _id: 'some-user-id' });
-    // expect(insertedUser).toEqual(mockUser);
+  it('should be truthy', async () => {
+    expect(connection).toBeTruthy();
   });
+
+  it('should be connected', async () => {
+    const users = db.collection('users');
+    expect(db).toBeTruthy();
+  });
+
+  // const users = db.collection('users');
+  // const mockUser = { _id: 'some-user-id', name: 'John' };
+  // await users.insertOne(mockUser);
+  // const insertedUser = await users.findOne({ _id: 'some-user-id' });
+  // expect(insertedUser).toEqual(mockUser);
+  // });
 });
